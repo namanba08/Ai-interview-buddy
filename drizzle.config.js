ab46ccb6df131bc64fe76c56e295124d3758566d
NAMAN BATRA <naman@Namans-MacBook-Air.local>
2024-09-26 20:12:54 +0530

    upload

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+	schema: "./utils/schema.js",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: "postgresql://neondb_owner:lYsEWUM1c7Ar@ep-spring-feather-a5snk0i5.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require",
+	},
+};
